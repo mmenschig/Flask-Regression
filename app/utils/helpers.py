4b1cb713c1cb10e1c491631a3591376952202299
mmenschig <marian.menschig@googlemail.com>
2018-09-16 16:31:56 -0700

    helper functions

---
+++ app/utils/helpers.py
@@ -0,0 +1,18 @@
+from time import time
+from config import ALLOWED_EXTENSIONS
+
+def allowed_file(filename):
+    """
+    Checks if the file extension is a valid one.
+    Currently supported are '.csv' and '.txt'
+    """
+    return "." in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def get_milliseconds_since_epoch():
+    """ Returns milliseconds elapsed since Jan 01 1970. """
+    return int(round(time() * 1000))
+
+def purge_uploads_folder():
+    # Do this via cron or everytime a chart has been generated?
+    pass
