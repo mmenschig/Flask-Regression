b6aebcf3b23d0b547e669611c93620f515a25cbc
mmenschig <marian.menschig@googlemail.com>
2018-09-16 16:30:24 -0700

    added upload logic and created additional routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,28 +1,63 @@
 
 import os
 from flask import Flask
-from flask import request, render_template, current_app
+from flask import request, render_template, flash, redirect, url_for, send_file
+from time import time
+from werkzeug.utils import secure_filename
+from app.utils.helpers import allowed_file
+
+from app.statistics import linear
+
 
 app = Flask(__name__)
 app.config.from_pyfile(os.path.join(os.pardir, 'config.py'))
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    print(request)
+    # flash(message="Invalid form", category="alert-danger")
+    if request.method == 'POST':
+        file = request.files['inputFile']
+        # TODO: flash message if file not provided
+
+        # TODO: fix message flashing
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            timestamp = int(round(time() * 1000))
+            filename = "{}_{}".format(timestamp,secure_filename(file.filename)) # Make this unix timestamp
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # TODO: call chart creation here
+            linear.main(timestamp=timestamp, filename=filename)
+            return redirect(url_for('statistics', chartId=timestamp))
+            # TODO: after upload, we need to generate chart, and redirect to chart-url
+    # GET Method
     return render_template("upload.html")
 
-@app.route('/chart/<chartId>', methods=['GET'])
-def chart():
 
-    return ""
+@app.route('/statistics/<int:chartId>', methods=['GET'])
+def statistics(chartId):
+    return render_template("statistics.html", chartId=chartId)
 
-# TODO: for later
-@app.route('/history/<pageId>', methods=['GET'])
-def history(pageId):
-    # Keep the last 100 charts?
-    return render_template('history.html', pageId=pageId)
+
+@app.route('/chart/<int:chartId>', methods=['GET'])
+def chart(chartId):
+    path = 'charts/{}.png'.format(chartId)
+    return send_file(path, mimetype='image/png')
+
+
+# Error Handlers
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("errors/404.html"), 404
+
+@app.errorhandler(500)
+def internal_server_error():
+    return render_template("errors/500.html"), 500
