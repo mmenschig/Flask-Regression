8ae582e4ff226b7b7f995e3e40a72a9e50ca1a7b
mmenschig <marian.menschig@googlemail.com>
2018-09-23 22:14:44 -0700

    update config with mail setting and secret key

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
-SECRET_KEY          = 'calilola' # set this via env var
+import os
+
+SECRET_KEY          = os.getenv('SECRET_KEY')
 
 UPLOAD_FOLDER       = 'app/uploads'
 CHART_FOLDER        = 'app/charts'
 MAX_CONTENT_PATH    = 4096
 MAX_FILE_SIZE       = 4096
 ALLOWED_EXTENSIONS  = set(['txt', 'csv'])
+
+# Mail Settings
+MAIL_SERVER         = 'smtp.gmail.com'
+MAIL_PORT           = 587
+MAIL_USE_TLS        = True
+MAIL_USE_SSL        = False
+MAIL_USERNAME       = os.getenv('MAIL_USERNAME')
+MAIL_PASSWORD       = os.getenv('MAIL_PASSWORD')
